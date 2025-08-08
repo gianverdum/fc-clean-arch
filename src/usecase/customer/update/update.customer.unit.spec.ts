49b05fbbbb3040b5d94e7760c03214bc220a0d23
gianverdum <gian.verdum@gmail.com>
2025-08-08 15:58:12 -0300

    Teste criado para criar use case de alterar customer

---
+++ src/usecase/customer/update/update.customer.unit.spec.ts
@@ -0,0 +1,38 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-object/address";
+
+const customer = CustomerFactory.createWithAddress(
+    'Jhon',
+    new Address('Street', 123, 'Zip', 'City')
+);
+
+const input = {
+    id: customer.id,
+    name: 'Jhon Updated',
+    address: {
+        street: 'Street Updated',
+        number: 456,
+        zip: 'Zip Updated',
+        city: 'City Updated'
+    },
+};
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        findAll: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+        update: jest.fn(),
+    };
+};
+
+describe('Unit Test for customer update use case', () => {
+    it('should update a customer', async () => {
+        const customerRepository = MockRepository();
+        const customerUpdateUseCase = new UpdateCustomerUseCase(customerRepository);
+
+        const output = await customerUpdateUseCase.execute(input);
+
+        expect(output).toEqual(input);
+    });
+});
